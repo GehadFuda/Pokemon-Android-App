fb0ce004eed4f1f4ba907a78915269294ae42b40
GehadFuda <30567417+GehadFuda@users.noreply.github.com>
2020-12-23 00:38:04 +0200

    First commet

---
+++ app/src/main/java/com/example/pokemon/di/DataBaseModule.java
@@ -0,0 +1,35 @@
+package com.example.pokemon.di;
+
+import android.app.Application;
+
+import androidx.room.Room;
+
+import com.example.pokemon.db.PokemonDao;
+import com.example.pokemon.db.PokemonDataBase;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+import dagger.hilt.InstallIn;
+import dagger.hilt.android.components.ApplicationComponent;
+
+@Module
+@InstallIn(ApplicationComponent.class)
+public class DataBaseModule {
+
+    @Provides
+    @Singleton
+    public static PokemonDataBase provideDataBase(Application application) {
+        return Room.databaseBuilder(application, PokemonDataBase.class, "fav_DB")
+                .fallbackToDestructiveMigration()
+                .allowMainThreadQueries()
+                .build();
+    }
+
+    @Provides
+    @Singleton
+    public static PokemonDao provideDao(PokemonDataBase pokemonDataBase) {
+        return pokemonDataBase.pokemonDao();
+    }
+}
